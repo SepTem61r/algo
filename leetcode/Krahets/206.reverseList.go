439540f67d660a42ba65be5184ea7a7e70c25061
SepTem61r <SepTem61r@example.com>
2026-06-28 14:18:50 +0800

    1

---
+++ leetcode/Krahets/206.reverseList.go
@@ -0,0 +1,13 @@
+package Krahets
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		nextTemp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = nextTemp
+	}
+	return prev
+}
